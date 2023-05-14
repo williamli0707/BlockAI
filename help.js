c1df662b5a0d8b1d8e3ead2adef19a1379ee48f7
William Li <williamhli0707@gmail.com>
2023-05-14 12:55:26 -0700

    transfer learning, fixed newlines

--- help.js
+++ help.js
@@ -15,22 +15,28 @@ let answers = [
     "Layers are parts of a neural network. They take information given to them, change them in some " +
         "way, and then pass it on to the next layer. Layers are important to AI because in our neural network, " +
         "for example, each layer will do different things which can change what output the model gives. " +
-        "\nSome common examples of layers are: " +
-        "\n    • Convolutional layer: Convolutional layers consist of a bunch of neurons, or pieces of the " +
+        "<br>Some common examples of layers are: " +
+        "<br>    • Convolutional layer: Convolutional layers consist of a bunch of neurons, or pieces of the " +
         "input given to them. Neurons in these layers will connect to neurons in the next layer, and each " +
         "neuron will have a weight which determines how important it is in predicting which class an image " +
         "belongs to. When the neural network trains itself, it will figure out which pieces of the image " +
         "are more important to the final result and adjust the weights accordingly. Stacking convolutional " +
         "layers is a common way to classify images. Early layers can learn things like lines, and later layers " +
         "can even learn how to distinguish shapes or objects. " +
-        "\n    • Pooling Layer: Pooling layers can simplify the input given to them. This is especially " +
+        "<br>    • Pooling Layer: Pooling layers can simplify the input given to them. This is especially " +
         "important because some layers contain extra or redundant information, and by simplifying them, models " +
         "can train faster. Another reason for pooling layers is that because convolutional layers have weights " +
         "for precise locations or pixels in an image, a slight shift in the image can drastically change the output. " +
         "Pooling layers help to downsample or downscale the input so that this effect is lessened. Some pooling " +
         "layers will take the average of an area given in the input, some will take the maximum (for example " +
         "MaxPooling2D), and so on. ",
-    "placeholder",
+    "Transfer learning is a machine learning technique that involves using knowledge gained from solving one problem to help solve a different but related problem. In transfer learning, a pre-trained model that has been trained on a large dataset and has learned useful feature representations is used as a starting point for a new task.<br>" +
+    "<br>" +
+    "Typically, the pre-trained model is trained on a large-scale dataset, such as ImageNet, which contains a vast number of images belonging to various categories. By training on such a dataset, the model learns to extract useful and general features that can be applied to many different visual recognition tasks. These learned features capture high-level representations of the data, which can be valuable for understanding different visual patterns.<br>" +
+    "<br>" +
+    "When applying transfer learning, the pre-trained model's learned feature representations are utilized as a foundation for a new task, which may have a smaller dataset or a different target domain. Instead of starting from scratch, the pre-trained model is fine-tuned or adapted using the new dataset specific to the target task. The idea is that the knowledge gained from the pre-training on the large dataset can provide a head start in learning the new task and improve its performance, even with limited training data.<br>" +
+    "<br>" +
+    "By leveraging transfer learning, the model can benefit from the general knowledge and feature extraction capabilities acquired during pre-training, saving time and computational resources compared to training a new model from scratch. Transfer learning has been successfully applied to various domains, including computer vision, natural language processing, and speech recognition, allowing models to achieve better performance and faster convergence on new tasks.",
     "placeholder",
     "placeholder",
     "placeholder"
@@ -56,8 +62,8 @@ for(let i = 0;i < n;i++) {
     collapse.setAttribute("data-bs-parent", "#help-accordion");
     aBody.classList.add("accordion-body");
 
-    button.textContent = questions[i];
-    aBody.textContent = answers[i];
+    button.innerHTML = questions[i];
+    aBody.innerHTML = answers[i];
 
     header.appendChild(button);
     collapse.appendChild(aBody);
